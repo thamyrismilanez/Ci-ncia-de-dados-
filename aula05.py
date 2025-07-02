facc7d53830db1a5dbb51bcb524b6510e77895d5
thamyrismilanez <thamyris.milanez@escola.pr.gov.br>
2025-07-02 13:51:00 +0000

    aula06medidas separatrizes parte 2

---
+++ aula05.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Conjunto de dados exemplo
+dados = [10, 20, 20, 30, 40, 50, 50, 50, 60, 70, 80, 90, 100]
+
+# Calcular quartis
+quartis = np.percentile(dados, [25, 50, 75])
+print(f'Quartis: Q1={quartis[0]}, Q2={quartis[1]}, Q3={quartis[2]}')
+
+# Calcular decis 
+decis = np.percentile(dados, [10, 20, 30, 40, 50, 60, 70, 80, 90])
+cis}')
